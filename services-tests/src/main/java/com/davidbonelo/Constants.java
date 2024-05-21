2dc81b598ebaeb6d27ee03a6783198a4d4ffce12
David Bonelo <bonelodev@gmail.com>
2024-05-21 17:58:30 -0500

    add soap isbn validation tests

--- services-tests/src/main/java/com/davidbonelo/Constants.java
+++ services-tests/src/main/java/com/davidbonelo/Constants.java
@@ -1,7 +1,9 @@
 package com.davidbonelo;
 
 public class Constants {
-    public static final String CALCULATOR_BASE = "http://www.dneonline.com/";
-    public static final String CALCULATOR_ENDPOINT = "calculator.asmx";
+    public static final String CALCULATOR_BASE = "http://www.dneonline.com";
+    public static final String CALCULATOR_ENDPOINT = "/calculator.asmx";
     public static final String BOOKSTORE_BASE = "https://demoqa.com";
+    public static final String DAE_BASE = "http://webservices.daehosting.com/services";
+    public static final String ISBN_ENDPOINT = "/isbnservice.wso";
 }
