5b1afdb25e8ffbde2a858e13c66e25eabcee1e6e
DanielMorales <dm34mg@gmail.com>
2024-05-21 20:22:04 -0500

    ArchivosUtil created

--- Automation5/src/test/java/co/com/demo/stepdefinitions/HelloStepDefinitions.java
+++ Automation5/src/test/java/co/com/demo/stepdefinitions/HelloStepDefinitions.java
@@ -1,4 +1,21 @@
 package co.com.demo.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class HelloStepDefinitions {
+    @Given("the Hello service is available")
+    public void theHelloServiceIsAvailable() {
+
+    }
+    @When("the user sends a request with {string}")
+    public void theUserSendsARequestWith(String string) {
+
+    }
+    @Then("I should receive a greeting with the name")
+    public void iShouldReceiveAGreetingWithTheName() {
+
+    }
+
 }
--- Automation5/src/test/resources/features/hello.feature
+++ Automation5/src/test/resources/features/hello.feature
@@ -1,12 +1,11 @@
 Feature: Hello Service
 
+  @Hello
   Scenario Outline: Provide a custom greeting
     Given the Hello service is available
     When the user sends a request with "<name>"
-    Then I should receive a greeting with the name "<name>"
+    Then I should receive a greeting with the name
 
     Examples:
       | name    |
       | Alice   |
-      | Bob     |
-      | Charlie |
